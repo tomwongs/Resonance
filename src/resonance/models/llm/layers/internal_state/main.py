d45be833be381664ffba6f898e7948785220c898
tomwongs <tomwongs.yoobee@gmail.com>
2026-06-10 14:07:51 +0200

    First implementation of internal_state layer.

--- src/resonance/models/llm/layers/internal_state/main.py
+++ src/resonance/models/llm/layers/internal_state/main.py
@@ -1,32 +1,58 @@
 
 class InternalState:
 
-    def __init__(self):
-        self.state = {
-            "mood": "neutral",
-            "valence": 0, # +1 positive, -1 negative
-            "arousal": 0
-        }
-        self.relationships = {
-            "user": {
-                "affection": 0,
-                "trust": 0,
-                "attachment": 0,
-                "comfort": 0
-            }
-        }
+    def __init__(self, emotions: dict, relationships: dict, activity: dict):
+        self.emotions = emotions
+        self.emotions_locked = [] 
+        self.relationships = relationships
+        self.activity = activity
 
-        self.activity = {
-            "focus": "user",
-            "energy": 0,
-            "social_drive": 0
-        }
+        #self.emotions_locked = {"happiness": True}     State that the happiness emotion cannot be changed, will stay locked.
 
-    def update_from_interaction(self):
-        pass
+        #self.emotions = {
+        #    "mood": "neutral",
+        #    "valence": 0, # +1 positive, -1 negative
+        #}
+        #self.relationships = {
+        #    "user": {
+        #        "affection": 0,
+        #        "trust": 0,
+        #        "attachment": 0,
+        #        "comfort": 0
+        #    }
+        #}
+        #
+        #self.activity = {
+        #    "focus": "user",
+        #    "energy": 0,
+        #    "social_drive": 0
+        #}
+
+    def lock_emotion(self, emotion: str):
+        if emotion in self.emotions and emotion not in self.emotions_locked:
+            self.emotions_locked.append(emotion)
+
+        return
+
+    def set_emotion(self, emotion: str, value):
+        if emotion in self.emotions:
+            if emotion in self.emotions_locked:
+                print(f"\x1b[33mEmotion \"{emotion}\" locked, cannot modify!\x1b[0m")
+            self.emotions[emotion] = value
+
+        return
+
+    def update_from_interaction(self, emotions={}):
+        if emotions:
+            for emotion, value in emotions.items():
+                self.set_emotion(emotion, value)
+
+        return self.emotions
+
 
     def decay_over_time(self):
         pass
+
 
     def compute_initiative(self):
         pass
